036dccc5e432df7c21eecd2b2c3df0e480c64fc2
Federico Pellegrini <fellegrini@gmail.com>
2023-02-07 00:17:36 -0300

    further config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,50 @@
 import { defineConfig } from 'vite';
-import path from 'path';
+import { resolve } from 'path';
+import { copy } from '@web/rollup-plugin-copy';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import summary from 'rollup-plugin-summary';
+
+const outDir = resolve(__dirname, 'dist');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@views': path.resolve(__dirname, './src/views'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@models': path.resolve(__dirname, './src/models'),
-      '@components': path.resolve(__dirname, '.src/components'),
+      '@': resolve(__dirname, 'src'),
+      '@assets': resolve(__dirname, 'src/assets'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@models': resolve(__dirname, 'src/models'),
+      '@views': resolve(__dirname, 'src/views'),
     },
   },
   build: {
+    outDir,
+    emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
+        // App entry points
+        main: resolve(__dirname, 'index.html'),
       },
-      external: /^lit/,
+      output: {
+        dir: 'dist',
+        format: 'cjs',
+      },
+      plugins: [
+        nodeResolve(),
+        terser({
+          module: true,
+          ecma: 2020,
+        }),
+        copy({
+          patterns: ['assets/images/*.{png,jpg,webp}'],
+          rootDir: 'src',
+          exclude: 'src/**/*.{ts,js}',
+        }),
+        summary(),
+      ],
+      external: '/^lit/',
     },
   },
 });
